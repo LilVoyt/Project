b2c5c993cc5ea7aa67a2c25e8a677a6554e0b7d2
Voyt <olegvoyt6@gmail.com>
2024-06-03 11:19:11 +0300

    ul working

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ actors.forEach(actor => {
     const actorCard = document.createElement('div');
     actorCard.className = 'actor-card';
 
+    const name = document.createElement('h2');
+    const fullName = `${actor.firstName} ${actor.lastName}`.trim();
+    name.textContent = fullName ? fullName : 'NoName';
+
     const img = document.createElement('img');
     img.src = actor.profilePicture ? actor.profilePicture : 'customer-photo/default-profile-picture.jpg';
     img.onerror = function() {
         this.src = 'customer-photo/default-profile-picture.jpg';
       };
     img.alt = `${actor.firstName} ${actor.lastName}`;
-
-    const name = document.createElement('h2');
-    const fullName = `${actor.firstName} ${actor.lastName}`.trim();
-    name.textContent = fullName ? fullName : 'NoName';
 
     const socialLinks = document.createElement('div');
     socialLinks.className = 'social-links';
@@ -49,19 +49,39 @@ function getSocialIcon(url) {
     return "icons/default.svg";
 }
 
-// const userPhotos = document.querySelectorAll(".actor-card img");
+let actorList = [];
 
-// userPhotos.forEach(photo => {
-//     photo.addEventListener("click", () => {
-//         alert("china");
-//     });
-// });
+const userPhotos = document.querySelectorAll(".actor-card img");
+const bottomList = document.querySelector('.bottom-list');
+const bottomListUl = bottomList.querySelector('ul');
 
-// const userName = document.querySelectorAll(".actor-card h2");
+userPhotos.forEach(photo => {
+    photo.addEventListener("click", () => {
+        const parentDiv = photo.closest('.actor-card');
+        const actorName = parentDiv.querySelector('h2').textContent;
+        if (actorList.includes(actorName)) {
+            alert('china');
+        } else {
+            actorList.push(actorName);
+            const actorNameli = document.createElement('h2');
+            actorNameli.textContent = actorName;
+            bottomListUl.appendChild(actorNameli);
+        }
+    });
+});
 
-// userName.forEach(name => {
-//     name.addEventListener("click", () => {
-//         alert("china");
-//     });
-// });
+const userName = document.querySelectorAll(".actor-card h2");
 
+userName.forEach(name => {
+    name.addEventListener("click", () => {
+        const actorName = name.closest('.actor-card');
+        if (actorList.includes(actorName)) {
+            alert('china');
+        } else {
+            actorList.push(actorName);
+            const actorNameli = document.createElement('li');
+            actorNameli.textContent = actorName;
+            bottomListUl.appendChild(actorNameli);
+        }
+    });
+});
